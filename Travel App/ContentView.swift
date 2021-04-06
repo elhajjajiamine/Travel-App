7b83b368ada53d5a3815728e66baa230e0b4973c
elhajjajiamine <aminselhajjaji@gmail.com>
2021-04-06 17:14:11 +0100

    Initial Commit

---
+++ Travel App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel App
+//
+//  Created by elhajjaji on 6/4/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by elhajjaji on 6/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
